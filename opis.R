7f5a5411f576f865867db337faff4f1f3b637d2f
Domen Mohorcic <d.mohorcic@gmail.com>
2020-08-27 18:48:05 +0200

    Napisan zaključek in alternative

--- opis.R
+++ opis.R
@@ -11,72 +11,11 @@ setwd('C:/Users/dmoho/Documents/FRI/2_letnik_1_semester/VS/podatki')
 stanovanja <- read.csv('stanovanja_vic.csv', sep = ";")
 
 # uredi podatke
-filtered <- select(stanovanja, 1, 4:6, 8:15, 17:19)
-data <- filtered[complete.cases(filtered), ]
-data <- transform(data, nadstropje = as.numeric(nadstropje))
-data_small <- select(data, 1,4,7,11,12,15)
+data <- stanovanja[complete.cases(stanovanja), ]
 
 # opisi podatke
-summary(data[2:15])
-describe(data[2:15])
-for(val in colnames(data[2:15])) {
-  shap <- shapiro.test(data[,val])
-  sym <- symmetry.test(data[,val])
-  printf("%s\n", val)
-  printf("shapiro.test\n")
-  printf("w = %f, p = %e\n", shap$statistic, shap$p.value)
-  printf("symmetry.test\n")
-  printf("Test statistics = %f, p = %e\n", sym$statistic, sym$p.value)
-  printf("%s\n\n", sym$alternative)
-}
-round(cor(data[2:12]), 3)
-pairs(data[2:12], lower.panel = NULL)
-
-for(val in colnames(data_small[2:6])) {
-  shap <- shapiro.test(data[,val])
-  sym <- symmetry.test(data[,val])
-  printf("%s\n", val)
-  printf("shapiro.test\n")
-  printf("w = %f, p = %e\n", shap$statistic, shap$p.value)
-  printf("symmetry.test\n")
-  printf("Test statistics = %f, p = %e\n", sym$statistic, sym$p.value)
-}
-
-# multipla regresija
-fit <- lm(skCena~nadstropje+vsaNadstropja+letoGradnje+stSob+stParkirisc+parkirisce+shramba+opremljenost+zunanjePovrsine+povrsina+oddaljenost, data = data)
-fit <- lm(skCena~letoGradnje+opremljenost+povrsina+oddaljenost+parkirisce, data = data)
-summary(fit)
-
-data_small$letoGradnje_SQ = data_small$letoGradnje*data_small$letoGradnje
-data_small$letoGradnje_SQRT = sqrt(data_small$letoGradnje)
-data_small$letoGradnje_LOG = log(data_small$letoGradnje)
-data_small$povrsina_SQ = data_small$povrsina*data_small$povrsina
-data_small$povrsina_SQRT = sqrt(data_small$povrsina)
-data_small$povrsina_LOG = log(data_small$povrsina)
-data_small$povrsina_EXP = exp(data_small$povrsina)
-data_small$oddaljenost_SQ = data_small$oddaljenost*data_small$oddaljenost
-data_small$oddaljenost_SQRT = sqrt(data_small$oddaljenost)
-data_small$oddaljenost_LOG = log(data_small$oddaljenost)
-
-pairs(select(data_small, 2,4:9), lower.panel = NULL)
-cor(select(data_small, 2,4:9))
-
-fit_small <- lm(skCena~povrsina, data=data_small)
-fit_small <- lm(skCena~letoGradnje+povrsina+oddaljenost+parkirisce, data = data_small)
-fit_small <- lm(skCena~letoGradnje+letoGradnje_SQ+letoGradnje_SQRT+letoGradnje_LOG
-                +povrsina+povrsina_SQ+povrsina_SQRT+povrsina_LOG
-                +oddaljenost+oddaljenost_SQ+oddaljenost_SQRT+oddaljenost_LOG
-                +parkirisce, data = data_small)
-fit_small <- lm(skCena~letoGradnje+povrsina+povrsina_EXP+oddaljenost+parkirisce, data = data_small)
-summary(fit_small)
-
-#reg <- regtabela(fit)
-#kex(reg)
-
-ggplot(data_small, aes(x=letoGradnje)) + geom_histogram(binwidth = 5, fill="#69b3a2")
-ggplot(data_small, aes(x=povrsina)) + geom_histogram(binwidth = 5, fill="#69b3a2")
-ggplot(data_small, aes(x=oddaljenost)) + geom_histogram(binwidth = 0.15, fill="#69b3a2")
-ggplot(data_small, aes(x=skCena)) + geom_histogram(binwidth = 80, fill="#69b3a2")
+summary(data)
+describe(data)
 
 par(mar=c(2,2,0,0))
 hist(data$letoGradnje, main="")
@@ -84,14 +23,95 @@ hist(data$povrsina, main="")
 hist(data$oddaljenost, main="")
 hist(data$skCena, main="")
 
-qqPlot(fit_small$residuals)
+round(cor(data), 3)
+pairs(select(data, 2, 4:6), lower.panel = NULL)
 
+shapiro.test(data$letoGradnje)
+symmetry.test(data$letoGradnje)
+shapiro.test(data$povrsina)
+symmetry.test(data$povrsina)
+shapiro.test(data$oddaljenost)
+symmetry.test(data$oddaljenost)
+shapiro.test(data$skCena)
+symmetry.test(data$skCena)
+
+# multipla regresija
+model <- lm(skCena~letoGradnje+povrsina+oddaljenost+parkirisce, data = data)
+summary(model)
+
+par(mar=c(2,2,2,2))
 par(mfrow=c(2,2))
-plot(fit_small, which=1:4)
-bptest(fit_small)
-influencePlot(fit_small)
+plot(model, which=1:4)
 
-a <- which(cooks.distance(fit_small) > 4/101)
-cooks.distance(fit_small)[a]>=qf(0.5, 5, 97)
+shapiro.test(model$residuals)
 
-ncvTest(fit_small)
+# isto
+ncvTest(model)
+bptest(model, studentize = FALSE)
+
+a <- which(cooks.distance(model) > 4/118)
+cooks.distance(model)[a]>=qf(0.5, 4, 115)
+
+b <- which(abs(rstandard(model)) > 2)
+cooks.distance(model)[b]>=qf(0.5, 4, 115)
+
+confint(model)
+
+# alternativni modeli
+
+data$letoGradnje_SQ = data$letoGradnje*data$letoGradnje
+data$letoGradnje_SQRT = sqrt(data$letoGradnje)
+data$letoGradnje_LOG = log(data$letoGradnje)
+data$letoGradnje_EXP = exp(data$letoGradnje)
+data$povrsina_SQ = data$povrsina*data$povrsina
+data$povrsina_SQRT = sqrt(data$povrsina)
+data$povrsina_LOG = log(data$povrsina)
+data$povrsina_EXP = exp(data$povrsina)
+data$oddaljenost_SQ = data$oddaljenost*data$oddaljenost
+data$oddaljenost_SQRT = sqrt(data$oddaljenost)
+data$oddaljenost_LOG = log(data$oddaljenost)
+data$oddaljenost_EXP = exp(data$oddaljenost)
+
+model_1 <- lm(skCena ~ letoGradnje_LOG+letoGradnje_SQRT+letoGradnje_SQ+letoGradnje+
+                          povrsina_EXP+povrsina_LOG+povrsina_SQRT+povrsina_SQ+povrsina+
+                          oddaljenost_EXP+oddaljenost_LOG+oddaljenost_SQRT+oddaljenost_SQ+oddaljenost+parkirisce, data = data)
+summary(model_1)
+par(mfrow=c(2,2))
+plot(model_1, which=1:4)
+
+
+data$povrsina_1 = data$povrsina
+data$povrsina_2 = data$povrsina_SQ
+data$povrsina_3 = data$povrsina_2*data$povrsina
+data$povrsina_4 = data$povrsina_3*data$povrsina
+data$povrsina_1_ = 1/data$povrsina_1
+data$povrsina_2_ = 1/data$povrsina_2
+data$povrsina_3_ = 1/data$povrsina_3
+data$povrsina_4_ = 1/data$povrsina_4
+
+model_2 <- lm(skCena ~ povrsina_4_+povrsina_3_+povrsina_2_+povrsina_1_+povrsina_1+povrsina_2+povrsina_3+povrsina_4, data = data)
+summary(model_2)
+par(mfrow=c(2,2))
+plot(model_2, which=1:4)
+
+
+model_3 <- lm(skCena ~ letoGradnje+povrsina_1+povrsina_2+oddaljenost+parkirisce, data = data)
+summary(model_3)
+par(mfrow=c(2,2))
+plot(model_3, which=1:4)
+
+data_small <- data[which(data$skCena < 1500),]
+model_4 <- lm(skCena ~ letoGradnje+povrsina+oddaljenost+parkirisce, data = data_small)
+summary(model_4)
+par(mfrow=c(2,2))
+plot(model_4, which=1:4)
+shapiro.test(model_4$residuals)
+ncvTest(model_4)
+
+
+anova(fit1, fit2) #primerja dva modela
+
+rstandard(fit1) #vec kot 2
+
+#reg <- regtabela(fit)
+#kex(reg)
